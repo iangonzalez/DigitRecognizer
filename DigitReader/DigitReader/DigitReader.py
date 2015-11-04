29286d45b512664807d2d4474f243a6eff64254e
Ian Gonzalez <gonzalez.isv@gmail.com>
2015-11-04 14:51:47 -0500

    Commented code.

--- DigitReader/DigitReader/DigitReader.py
+++ DigitReader/DigitReader/DigitReader.py
@@ -7,11 +7,15 @@ from sklearn.externals import joblib
 from sklearn.multiclass import OneVsRestClassifier
 from PIL import Image
 
+# class to handle reading in images as input
 class ImageReader:
     def __init__(self):
         self.image = None
 
     def readImgFromFile(self, fname):
+        """
+        Reads the image from a file and resizes it to the classifier's required size (27x27)
+        """
         self.image = Image.open(fname)
         self.image = self.image.resize((27,27))
 
@@ -19,6 +23,9 @@ class ImageReader:
         self.image = Image.frombuffer("RGBA", size, buf)
 
     def getPxData(self):
+        """
+        Returns the image data as an unrolled matrix of averaged pixel values.
+        """
         if self.image is not None:
             imageSize = self.image.size
 
@@ -30,22 +37,28 @@ class ImageReader:
         else:
             raise ValueError("Image hasn't been read in yet.")
 
+# Class to train and predict with the SVM image classifier.
 class DigitReader:
     def __init__(self, debug = False):
         self.svm_classifier = OneVsRestClassifier(SVC(kernel="linear"))
         self.dim_reducer = PCA(n_components=10)
-        self.trainData = []
         self.trainDataMatrix = None
         self.train_y = None
         self.trained = False
+
+        # debug mode restricts trains/tests to 100 data points
         self.debug = debug
 
     def getTrainingDataFromImages(self, fnames, labels):
+        """
+        Given image file names and labels, reads in training data set.
+        """
+        trainData = []
         for fname in fnames:
             imgReader = ImageReader()
             imgReader.readImgFromFile(fname)
-            self.trainData.append(imgReader.getPxData())
-        self.trainDataMatrix = np.array(self.trainData)
+            trainData.append(imgReader.getPxData())
+        self.trainDataMatrix = np.array(trainData)
         self.train_y = labels
 
     def getTrainingDataFromCsv(self, fname):
@@ -54,12 +67,15 @@ class DigitReader:
 
         if self.debug:
             trainingData = trainingData[1:100, :]
- 
+        
+        # set the feature matrix and the corresponding labels:
         self.train_y = trainingData[:, 0]
-        print(self.train_y)
         self.trainDataMatrix = trainingData[:, 1:]
 
     def reduceDimPCA(self, data):
+        """
+        Reduce dimensionality of data matrix (hardcoded to 10 components right now).
+        """
         return self.dim_reducer.fit_transform(data)
 
     def reduceDimTrainingData(self):
@@ -69,6 +85,8 @@ class DigitReader:
     def trainSVM(self, saveto=""):
         print("Training svm model.")
         self.svm_classifier.fit(self.trainDataMatrix, self.train_y)
+
+        # dump the svm to the given file for later load
         if saveto:
             joblib.dump(self.svm_classifier, saveto)
 
@@ -82,6 +100,10 @@ class DigitReader:
             return False
 
     def predictDigit(self, input_data, model_location="../classifier/digit_svm.skm"):
+        """
+        Return the predicted classes for given pixel input_data (unrolled 27x27 matrix).
+        Reads model from given location if classifier not already trained.
+        """
         if not self.trained:
             print("Loading model.")
             if not self.loadTrainedModel(model_location):
@@ -111,9 +133,11 @@ class DigitReader:
 
 
 
-
-if __name__ == '__main__':    
+# running the code from the command line:
+if __name__ == '__main__':
+    # -r flag retrains the model
     retrain = "-r" in sys.argv
+    # -i + file indicates input
     try:
         input_file_idx = sys.argv.index("-i") + 1
         if input_file_idx >= len(sys.argv):
@@ -125,12 +149,14 @@ if __name__ == '__main__':
 
     digReader = DigitReader(debug = True)
 
+    # run the model training with dimensionality reduction:
     if not os.path.isfile("../classifier/digit_svm.skm") or retrain:
         sys.stdout.write("Training the classifier and dumping result to classifier folder.\n")
         digReader.getTrainingDataFromCsv("../data/train.csv")
         digReader.reduceDimTrainingData()
         digReader.trainSVM("../classifier/digit_svm.skm")
 
+    # predict on the input file if it was given and dump to output.txt:
     if input_file is not None:
         print("Running predictions on the input file.")
         if input_file.endswith(".csv"):
